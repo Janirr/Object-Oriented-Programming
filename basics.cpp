8e9126c703400b4c743db23bd537281f3d4b5bd8
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-10-10 18:07:53 +0200

    Added info output for Player

--- basics.cpp
+++ basics.cpp
@@ -7,15 +7,33 @@ class Player{
 		int height;
 		int goals;
 		int assists;
+		void info();
+};
+
+void Player::info(){
+	cout << "Imię: " << name << endl;
+	cout << "Wiek: " << age << endl;
+	cout << "Wzrost: " << height << endl;
+	cout << "Gole: " << goals << endl;
+	cout << "Asysty: " << assists << endl;
+}
+		 
+class Club{
+	public:
+		string name;
+		int founded;
 };
 
 int main() {
 	Player lewandowski;
-	lewandowski.age = 34;
+	lewandowski.age = 32;
 	lewandowski.name = "Robert";
 	lewandowski.height = 185;
-	lewandowski.goals = 401;
-	lewandowski.assists = 103;
-	
+	lewandowski.goals = 264;
+	lewandowski.assists = 54;
+	lewandowski.info();
+	Club barcelona;
+	barcelona.name = "FC Barcelona";
+	barcelona.founded = 1899;
 	return 0;
 }
