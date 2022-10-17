6438c3a9f453182d64134c74d6dd85f28734a3e7
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-10-17 17:11:35 +0200

    void info is now inside of the class
    Also used a pointer Club& club instead of Club club

--- basics.cpp
+++ basics.cpp
@@ -3,39 +3,36 @@ using namespace std;
 
 class Club{
 	public:
-	string name;
-	int founded;
-	//constructor
-        Club(string name_, int founded_):
-        name{name_},
-        founded{founded_}{}
-	void info();
+		string name;
+		int founded;
+		//constructor
+        	Club(string name_, int founded_):
+                	name{name_},
+                	founded{founded_}{}
+		void info();
 };
 
 class Player{
 	public:
-	int age,height,goals;
-        string name, surname;
-        Club club;
-        //constructor
-        Player(int age_, int height_, int goals_, string name_, string surname_, Club club_):
-            age{age_},
-            height{height_},
-            goals{goals_},
-            name{name_},
-            surname{surname_},
-            club{club_}
-            {}
-        void info();
+	    int age,height,goals;
+        	string name, surname;
+        	Club& club;
+        	//constructor
+        	Player(int age_, int height_, int goals_, string name_, string surname_, Club& club_):
+            		age{age_},
+            		height{height_},
+		    	goals{goals_},
+		    	name{name_},
+		    	surname{surname_},
+		    	club{club_}{}
+	
+        	void info(){
+            		cout << name << " " << surname << " plays in " << club.name << " which was founded in " << club.founded << endl;
+            		};
 };
-
-void info(Player player){
-    cout << player.name << " " << player.surname << " plays in " << player.club.name << " which was founded in " << player.club.founded <<endl;
-}
 int main(){
-
     Club barcelona("FC Barcelona",1899);
     Player lewandowski(34,185,401,"Robert","Lewandowski",barcelona);
-    info(lewandowski);
-	return 0;
+    lewandowski.info();
+    return 0;
 }
