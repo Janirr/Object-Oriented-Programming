d57ab26c5702e129b4927fbbf13b885b3dff2ba0
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-11-28 18:09:36 +0100

    Create ex-1.cpp

---
+++ lesson-6/ex-1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+class Number{
+    public: 
+    int a;
+    Number(int a_):a{a_}{}
+    double doubleValue(int a){return double(a);}
+};
+class Comparison{
+private:
+    int a;
+    int b;
+public:
+Comparison(int a, int b){this->a=a;this->b=b;}
+  int max()
+  {
+      return a > b ? a : b;
+  }
+  int min()
+  {
+      return a < b ? a : b;
+  }
+  float avg()
+  {
+      return ((a + b) / 2);
+  }
+  
+};
+class Logarithm{
+private:
+    double base, argument;
+public:
+    double doubleValue() { if (this->argument < 0){if (this->base < 0){return -1;}}{return log(this->argument) / log(this->base);}}
+    Logarithm(double inputBase, double inputArgument) {
+        if (inputBase > 0){this->base = inputBase;}
+        else {this->base = -1 * inputBase;}
+        if (inputArgument > 0){this->argument = inputArgument;}
+        else {this->argument = -1 * inputArgument;}
+        
+    }
+};
+int main()
+{
+    Number a(5);
+    Number b(6);
+    Comparison c(5,7);
+    Logarithm d(-5,-7);
+    cout << d.doubleValue() << endl;
+  cout << c.max() << endl;
+  cout << c.min() << endl;
+  cout << c.avg() << endl;
+  return 0;
+}
