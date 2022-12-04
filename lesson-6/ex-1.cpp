3f9fa7bc49120823e8350980bd048cbd2379ea8e
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-12-04 21:00:07 +0100

    task1: avg now works with floats too

--- lesson-6/ex-1.cpp
+++ lesson-6/ex-1.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
-class Number{
-    public: 
-    int a;
-    Number(int a_):a{a_}{}
-    double doubleValue(int a){return double(a);}
-};
 class Comparison{
 private:
     float a;
     float b;
 public:
-Comparison(int a, int b){this->a=a;this->b=b;}
+Comparison(float a, float b){this->a=a;this->b=b;}
   int max()
   {
       return a > b ? a : b;
@@ -28,31 +21,13 @@ Comparison(int a, int b){this->a=a;this->b=b;}
   }
   
 };
-class Logarithm{
-private:
-    double base, argument;
-public:
-    double doubleValue() { if (this->argument < 0){if (this->base < 0){return -1;}}{return log(this->argument) / log(this->base);}}
-    Logarithm(double inputBase, double inputArgument) {
-        if (inputBase > 0){this->base = inputBase;}
-        else {this->base = -1 * inputBase;}
-        if (inputArgument > 0){this->argument = inputArgument;}
-        else {this->argument = -1 * inputArgument;}
-        
-    }
-};
+
 int main()
 {
-    Number a(5);
-    Number b(6);
-
-    Comparison c(5,6)
+    Comparison c(5.004f,6.9f);
     cout << c.max() << endl;
     cout << c.min() << endl;
     cout << c.avg() << endl;
-
-    Logarithm d(-5,-7);
-    cout << d.doubleValue() << endl;
     
     return 0;
 }
