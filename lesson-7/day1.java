7a82b9d868fe4d3d33e3baf3fa94e14037546697
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2022-12-05 17:51:31 +0100

    Update day1.java

--- lesson-7/day1.java
+++ lesson-7/day1.java
@@ -7,25 +7,20 @@ public class Day1 {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(new File("day1.txt"));
-            int s = 0;
-            int max_s = 0;
-            int elf_nr = 0;
-            int elf_max = 0;
+            int kcal = 0,max_kcal = 0,elf_nr = 0,elf_max = 0;
             while (scanner.hasNextLine()) {
                 String x = scanner.nextLine();
                 if (x.isEmpty()){
                     elf_nr += 1;
-                    if (s > max_s) {
-                        elf_max = elf_nr;
-                        max_s=s;
-                    }
-                    s = 0;
+                    Comparison c1 = new Comparison(kcal,max_kcal);
+                    if (c1.isBigger()){elf_max = elf_nr; max_kcal=kcal; }
+                    kcal = 0;
                 }
                 else{
-                    s+=Integer.parseInt(x);
+                    kcal+=Integer.parseInt(x);
                 }
             }
-            System.out.println("elf nr "+elf_max+" consumed "+max_s+" calories.");
+            System.out.println("elf nr "+elf_max+" consumed "+max_kcal+" calories.");
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
