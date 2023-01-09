5bae6ec45e4e962bc6746df408bcb746c411e000
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2023-01-09 18:00:39 +0100

    Create Main.java

---
+++ lesson-10/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Shiritori my_shiritori = new Shiritori();
+        my_shiritori.play("apple");
+        my_shiritori.play("ear");
+        my_shiritori.play("rhino");
+        my_shiritori.play("corn");
+        my_shiritori.printWords();
+        my_shiritori.restart();
+        my_shiritori.play("hostess");
+        my_shiritori.play("stash");
+        my_shiritori.play("hostess");
+        my_shiritori.play("hamilton");
+        my_shiritori.printWords();
+    }
+}
