386c07efda409a16e574f528f80a26d36e3c3869
Jakub Januszewski <79792011+Janirr@users.noreply.github.com>
2023-01-16 17:57:40 +0100

    Changed Developer to not end with -er

--- lesson-1/ex-1.cpp
+++ lesson-1/ex-1.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
 
-class Developer{
+class Develope{
 	public:
 		std::string name;
 		int founded;
-		Developer(){this->name=name;this->founded=founded;}
-		Developer(std::string name_, int founded_):name{name_},founded{founded_}{}
+		Develope(){this->name=name;this->founded=founded;}
+		Develope(std::string name_, int founded_):name{name_},founded{founded_}{}
 };
 
 class Game{
 	private:
 		std::string name;
 		float priceUSD;
-		Developer &developer; //private access to the class
+		Develope &develope; //private access to the class
 		int players;
 	public:
 		//Game(){}
-		Game(std::string name_,float priceUSD_,Developer &developer_,int players_):name{name_},priceUSD{priceUSD_},developer{developer_},players{players_}{}
+		Game(std::string name_,float priceUSD_,Develope &develope_,int players_):name{name_},priceUSD{priceUSD_},develope{develope_},players{players_}{}
 		void info(){std::cout<<"Name: "<<name<<" | Price: "<<priceUSD<<" $ | Players: "<<players<<std::endl;}
-		void devinfo(){std::cout<<"Developer: "<<developer.name<<" founded in: "<<developer.founded;}
+		void devinfo(){std::cout<<"Develope: "<<develope.name<<" founded in: "<<developer.founded;}
 };
 
 int main(){
-	Developer EA("EA SPORTS",1982);
+	Develope EA("EA SPORTS",1982);
 	Game fifa21("FIFA 21",49.99f,EA,986);
 	fifa21.info();
 	fifa21.devinfo();
